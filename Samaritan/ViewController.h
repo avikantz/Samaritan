ad477bfc21a84d0b087d049477a7a84f6b9d3b73
avikantz <avikantsainidbz@gmail.com>
2015-11-29 18:09:32 +0530

    Initial Commit

---
+++ Samaritan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Samaritan
+//
+//  Created by Avikant Saini on 11/29/15.
+//  Copyright © 2015 Dark Army. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Samaritan/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Samaritan
+//
+//  Created by Avikant Saini on 11/29/15.
+//  Copyright © 2015 Dark Army. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
